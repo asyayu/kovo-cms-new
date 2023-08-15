f63e0b93cab924fd0d37e1a1cfd7113253b905b5
asyayu <yurchenkoasya@gmail.com>
2023-08-15 14:55:22 +0200

    Update the download version

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
-const Course = require("../models/course");
-const {semesters, semSansSpace} = require('../config/data');
+const Course = require('../models/course');
+const { semesters, semSansSpace } = require('../config/data');
 
 module.exports = {
     getIndex: (req, res) => {
-        res.render('home/index.ejs', {title: 'KoVo CMS'});
+        res.render('home/index.ejs', { title: 'KoVo CMS' });
     },
     getKoVo: async (req, res) => {
         try {
@@ -15,15 +15,21 @@ module.exports = {
             }
             let uniqueModules = [...new Set(diffModules)];
             uniqueModules.sort();
-            res.render('home/kovo.ejs', {courses, semesters, uniqueModules, semSansSpace, title: 'All Courses'})
+            res.render('home/kovo.ejs', {
+                courses,
+                semesters,
+                uniqueModules,
+                semSansSpace,
+                title: 'All Courses',
+            });
         } catch (err) {
             console.log(err);
-            res.render('error', {err, title: 'Error'});
+            res.render('error', { err, title: 'Error' });
         }
     },
     getHTML: async (req, res) => {
         const html = await axios.get('http://localhost:4000/kovo');
         const htmlCode = html.data.replace(/\s+/g, ' ');
-        res.render('home/publish.ejs', {htmlCode, title: 'Download HTML'});
-    }
+        res.render('home/publish.ejs', { htmlCode, title: 'Download HTML' });
+    },
 };
